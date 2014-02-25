008a31eb9ecc643f32494b331d6ba33b6213b0d7
Student1 <Student1@pc-05>
2014-02-25 11:25:31 +0300

    DB minor changes

--- java dee/src/com/itechart/phonny/model/dao/databaseimpl/PhoneRecordDAOImpl.java
+++ java dee/src/com/itechart/phonny/model/dao/databaseimpl/PhoneRecordDAOImpl.java
@@ -1,5 +1,9 @@
 package com.itechart.phonny.model.dao.databaseimpl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -9,14 +13,18 @@ import com.itechart.phonny.model.entity.PhoneRecord;
 
 
 public class PhoneRecordDAOImpl implements PhoneRecordDAO {
-    
-    private List<PhoneRecord> records;
-    
+	
+	private static final String GET_RECORDS_SHORT_QUERY = 
+			"SELECT first_name, patronymic, surname, birthdate, " +
+			"email, organization, country, city, street, house_number, appartment_number " +
+			"FROM (jdee.contact JOIN jdee.address) JOIN jdee.workplace " +
+			"WHERE contact.address = address.id AND contact.workplace = workplace.id LIMIT ?, ?;";
+        
     public PhoneRecordDAOImpl() {
 
         super();
         // TODO: replace with database implementation
-        records = new ArrayList<>();
+        /*records = new ArrayList<>();
         PhoneRecord record = new PhoneRecord();
         record.setFirstName("Anna");
         record.setLastName("HelloWorld");
@@ -26,7 +34,7 @@ public class PhoneRecordDAOImpl implements PhoneRecordDAO {
         records.add(record);
         records.add(record);
         records.add(record);
-        records.add(record);
+        records.add(record);*/
     }
 
     @Override
@@ -36,12 +44,58 @@ public class PhoneRecordDAOImpl implements PhoneRecordDAO {
     }
 
     @Override
-    public List<PhoneRecord> getRecords() {
+    public List<PhoneRecord> getRecords(int first, int last) {
 
+    	List<PhoneRecord> records = new ArrayList<PhoneRecord>();
+    	Connection connection = null;
+    	PreparedStatement statement = null;
+    	try {
+			connection = StorageProvider.INSTANCE.getConnection();
+			statement = connection.prepareStatement(GET_RECORDS_SHORT_QUERY);
+			ResultSet resultSet = executeRecordsStatement(first, last,
+					statement);
+			parseRecordsSet(resultSet, records);
+			
+		} catch (SQLException e) {
+			// TODO log
+			e.printStackTrace();
+		} finally {
+			closeResources(connection, statement);
+		}
         return records;
     }
 
-    @Override
+	private ResultSet executeRecordsStatement(int first, int last,
+			PreparedStatement statement) throws SQLException {
+		statement.setInt(1, first);
+		statement.setInt(2, last);
+		ResultSet resultSet = statement.executeQuery();
+		return resultSet;
+	}
+
+    private void parseRecordsSet(ResultSet resultSet, List<PhoneRecord> records)
+    							throws SQLException {
+    	
+		while(resultSet.next()) {
+			
+		}
+	}
+
+	private void closeResources(Connection connection,
+			PreparedStatement statement) {
+		try {
+			if(statement != null) {
+				statement.close();
+			}
+			if(connection != null) {
+				connection.close();
+			}
+		} catch(SQLException e) {
+			// TODO log
+		}
+	}
+
+	@Override
     public void update(PhoneRecord record) {
 
         
--- java dee/src/com/itechart/phonny/model/dao/databaseimpl/StorageProvider.java
+++ java dee/src/com/itechart/phonny/model/dao/databaseimpl/StorageProvider.java
@@ -1,6 +1,51 @@
 package com.itechart.phonny.model.dao.databaseimpl;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 
-public class StorageProvider {
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.sql.DataSource;
 
+
+public enum StorageProvider {
+	
+	INSTANCE;
+	
+	// TODO: move to .properties file
+	
+	private static final String JNDI_CONTEXT = "java:comp/env";
+	
+	private static final String JNDI_DB_POOL = "jdbc/jdee";
+	
+	private static final String DB_CANNOT_CONNECT_MESSAGE = "Couldn't find Datasource";
+	
+	private DataSource pool;
+	
+	private StorageProvider() {
+		
+		init();
+	}
+
+	private void init() {
+
+		Context env = null;
+		try {
+			env = (Context) new InitialContext().lookup(JNDI_CONTEXT);
+			pool = (DataSource) env.lookup(JNDI_DB_POOL);
+			if(pool == null) {
+                throw new ServletException(DB_CANNOT_CONNECT_MESSAGE);
+            }			
+		} catch (NamingException|ServletException e) {
+			// TODO: add logger.error
+			System.out.println(e);
+		}
+	}
+	
+	public Connection getConnection() throws SQLException {
+		
+		return pool.getConnection();
+	}
 }
